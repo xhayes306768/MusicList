692cf9e203778af2561f610c7296328547658c3f
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-14 09:11:39 -0500

    Created the Music Context classs that inherits the DbContext class.

--- MusicList/Models/Music.cs
+++ MusicList/Models/Music.cs
@@ -1,6 +1,42 @@
-﻿namespace MusicList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicList.Models
 {
     public class Music
     {
+        // EF Core will configure the database to generate this value
+
+        public int MusicId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the artist.")]
+        public string Artist { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+
+        // Add a collection to store songs associated with the music
+        public ICollection<Song> Songs { get; set; }
+
+
     }
+    public class Song
+    {
+        public int SongId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a song title.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the duration.")]
+        public TimeSpan Duration { get; set; }
+
+    }
+
 }
---
+++ MusicList/Models/MusicContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MusicList.Models
+{
+    public class MusicContext: DbContext
+    {
+        
+        
+            public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+            { }
+            public DbSet<Music> Movies { get; set; } = null!;
+        
+
+
+
+    }
+}
