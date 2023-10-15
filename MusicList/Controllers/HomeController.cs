c4a1fbd461c28c3882b88d1b4e964cb9477345cb
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-10-14 23:38:53 -0500

    Modified the home controller.

--- MusicList/Controllers/HomeController.cs
+++ MusicList/Controllers/HomeController.cs
@@ -1,32 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MusicList.Models;
-using System.Diagnostics;
+using System.Linq;
 
 namespace MusicList.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private MusicContext context { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MusicContext ctx)
         {
-            _logger = logger;
+            context = ctx;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var music = context.Music.OrderBy(m => m.Title).ToList();
+            return View(music);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-       // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-      //  public IActionResult Error()
-      //  {
-          //  return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-      //  }
     }
 }
